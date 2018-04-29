e2ebfe727079ba3b1063814b7b4fea192d678465
jujuying <jujuying@gmail.com>
2018-04-29 10:19:24 +0800

    how to download score information

---
+++ src/crawler/example/past/ScorePagesExample.java
@@ -0,0 +1,44 @@
+package crawler.example.past;
+
+import com.github.abola.crawler.CrawlerPack;
+import org.apache.commons.logging.impl.SimpleLog;
+
+/**
+ * 爬蟲包程式的全貌，就只有這固定的模式
+ * 
+ * @author Abola Lee
+ *
+ */
+public class ScorePagesExample {
+	// commit test test
+	public static void main(String[] args) {
+
+		// set to debug level
+		//CrawlerPack.setLoggerLevel(SimpleLog.LOG_LEVEL_DEBUG);
+
+		// turn off logging
+		CrawlerPack.setLoggerLevel(SimpleLog.LOG_LEVEL_OFF);
+
+		// 遠端資料路徑
+		String uri = "http://www.ipeen.com.tw/comment/1621785";
+		System.out.println(
+				CrawlerPack.start().getFromHtml(uri).select("#comment > header > div > div > div.brief > p:nth-child(1)"));
+
+		System.out.println(
+				CrawlerPack.start()
+				
+				// 參數設定
+			    //.addCookie("key","value")	// 設定cookie
+				//.setRemoteEncoding("big5")// 設定遠端資料文件編碼
+				
+				// 選擇資料格式 (三選一)
+				//.getFromJson(uri)
+			    .getFromHtml(uri)
+			    //.getFromXml(uri)
+			    
+			    // 這兒開始是 Jsoup Document 物件操作
+			    .select("#comment > header > div > div > div.brief > div > dl")
+
+		);
+	}
+}
